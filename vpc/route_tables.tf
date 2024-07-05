c1a764d1b814a32ff81394d2f9340be1e5089bc1
Marcio Machado <mmm2004.marcio@gmail.com>
2024-07-04 23:51:03 -0300

    Update route_tables.tf

--- vpc/route_tables.tf
+++ vpc/route_tables.tf
@@ -20,6 +20,20 @@ resource "aws_route_table" "private" {
   }
 }
 
+resource "aws_route_table" "natgw" {
+  count = var.create_nat_gateway ? length(var.subnet_indices_for_nat) : 0
+  vpc_id = var.vpc
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = element(aws_nat_gateway.nat_gateway.*.id, count.index)
+  }
+
+  tags = {
+    Name = "natgw-${var.route_table_tag}-${count.index + 1}"
+  }
+}
+
 
 // Route Table association
 resource "aws_route_table_association" "public" {
@@ -33,4 +47,10 @@ resource "aws_route_table_association" "private" {
   count          = var.count_available_subnets
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = element(aws_route_table.private.*.id, count.index)
+}
+
+resource "aws_route_table_association" "natgw" {
+  count = var.create_nat_gateway ? length(var.subnet_indices_for_nat) : 0
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_nat_gateway.nat_gateway.*.id, count.index)
 }
